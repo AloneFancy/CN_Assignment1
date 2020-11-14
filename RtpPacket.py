67186b8318069a6f5ae0734a472c2b267de7848c
binhtranit <binhtran1832@gmail.com>
2020-11-14 10:12:50 +0700

    modify RptPacket.py

--- RtpPacket.py
+++ RtpPacket.py
@@ -17,9 +17,22 @@ class RtpPacket:
 		#--------------
 		# Fill the header bytearray with RTP header fields
 		
-		# header[0] = ...
-		# ...
-		
+		header[0] = version << 6
+		header[0] |= padding << 5
+		header[0] |= extension << 4
+		header[0] |= cc
+		header[1] = (marker << 7) | pt
+		header[2] = (seqnum >> 8) & 255
+		header[3] = seqnum & 255
+		header[4] = (timestamp >> 24) & 255
+		header[5] = (timestamp >> 16) & 255
+		header[6] = (timestamp >> 8) & 255
+		header[7] = timestamp & 255
+		header[8] = (ssrc >> 24) & 255
+		header[9] = (ssrc >> 16) & 255
+		header[10] = (ssrc >> 8) & 255
+		header[11] = ssrc & 255
+
 		# Get the payload from the argument
 		# self.payload = ...
 		
